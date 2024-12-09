4baac43581ed507cb0b0712897d3cdf54c11bffb
asifalam515 <asibulalam515@gmail.com>
2024-12-09 21:37:54 +0600

    meta data added

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const metadata = {
+  title: "About page",
+  description: "all bout about",
+};
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="text-5xl">This is About page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+export const metadata = {
+  title: "HomePage",
+  description: "This is homepage",
+};
 const Page = () => {
   return (
     <div>
