18d2f5d5f8957e231a9558bfac439a1ca7791faf
asifalam515 <asibulalam515@gmail.com>
2024-12-17 10:13:41 +0600

    card added

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,33 @@ export const metadata = {
   title: "HomePage",
   description: "This is homepage",
 };
-const Page = () => {
+const Page = async () => {
+  const res = await fetch("http://localhost:5000/shoes");
+  const shoes = await res.json();
+  // console.log(data);
   return (
     <div>
       <h1 className="text-center text-5xl text-red-400">welcome to next js</h1>
+
+      <div className="grid grid-cols-3 gap-5">
+        {shoes.map((shoe) => (
+          <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{shoe.title}</h2>
+              <p>{shoe.description}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
